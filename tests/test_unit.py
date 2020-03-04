d0b48d185a8260c26d8eb87a40b10370c0a62ad8
Alan Justino <alan.justino@yahoo.com.br>
2020-03-04 17:06:53 -0300

    Use a public S3 bucket for testing
    For now, Minio provides one for free.
    Minio aims to be AWS S3 compatible, so it will do for now

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -26,9 +26,22 @@ def test_filesystem_storages_honor_workdir():
     assert filename in os.listdir(workdir), 'File is not on the storage workdir'
 
 
+# From: https://docs.minio.io/docs/aws-cli-with-minio.html
+MINIO_S3_SETTINGS = {
+    'AWS_ACCESS_KEY_ID': 'Q3AM3UQ867SPQQA43P2F',
+    'AWS_SECRET_ACCESS_KEY': 'zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG',
+    'AWS_S3_REGION_NAME': 'us-east-1',
+    'AWS_S3_ENDPOINT_URL': 'https://play.min.io:9000',
+    'AWS_S3_SIGNATURE_VERSION': 's3v4',
+}
+
+
 @pytest.mark.parametrize("storage_class, storage_params", [
     (storages.TemporaryFilesystemStorage, {}),
-    (storages.S3Storage, {'workdir': 's3://gn-ninja/storage-test/'}),
+    (storages.S3Storage, {
+        'settings': MINIO_S3_SETTINGS,
+        'workdir': 's3://nondjango-storages-test/storage-test/'
+    }),
 ])
 def test_file_read_write(storage_class, storage_params):
     payload = 'test payload'
